5ad006c63b13c33f197637c97bed5f0f3461311b
SvRgK11010 <konnova-s.s15@yandex.ru>
2023-03-26 17:10:17 +0300

    5 spr

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -9,23 +9,8 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        auto results = search_server_.FindTopDocuments(raw_query, document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
-        if (results.empty()) {
-            ++no_result_requests_;
-            if (no_result_requests_ > min_in_day_) {
-                --no_result_requests_;
-            }
-        }
-        requests_.push_back({ static_cast<int>(results.size()) });
-        if (requests_.size() > min_in_day_) {
-            requests_.pop_front();
-            --no_result_requests_;
-        }
-        return results;
-
-    }
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query);
@@ -40,3 +25,22 @@ private:
     const static int min_in_day_ = 1440;
     const SearchServer& search_server_;
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    auto results = search_server_.FindTopDocuments(raw_query, document_predicate);
+
+    if (results.empty()) {
+        ++no_result_requests_;
+        if (no_result_requests_ > min_in_day_) {
+            --no_result_requests_;
+        }
+    }
+    requests_.push_back({ static_cast<int>(results.size()) });
+    if (requests_.size() > min_in_day_) {
+        requests_.pop_front();
+        --no_result_requests_;
+    }
+    return results;
+
+}
